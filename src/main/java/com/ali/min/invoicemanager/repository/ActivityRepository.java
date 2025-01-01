b77222f80cb7a2545ef65549d1d75f76c55a04ad
aliminn <aliminnet@gmail.com>
2025-01-01 18:29:58 +0300

    add activity operations

--- src/main/java/com/ali/min/invoicemanager/repository/ActivityRepository.java
+++ src/main/java/com/ali/min/invoicemanager/repository/ActivityRepository.java
@@ -1,12 +1,10 @@
 package com.ali.min.invoicemanager.repository;
 
 import com.ali.min.invoicemanager.entity.ActivityEntity;
-import com.ali.min.invoicemanager.entity.ProductEntity;
 import com.ali.min.invoicemanager.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ActivityRepository extends JpaRepository<ActivityEntity, Long> {
     List<ActivityEntity> findAllByStatus(Status status);
